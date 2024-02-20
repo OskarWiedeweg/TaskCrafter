56fb4b9000bce8002240df500df1b021793a68b3
OskarWiedeweg <dev.wiedeweg@gmail.com>
2024-02-20 09:29:27 +0100

    initial: backend

---
+++ backend/src/test/java/com/oskarwiedeweg/taskcrafterbackend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oskarwiedeweg.taskcrafterbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
